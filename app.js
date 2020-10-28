f35e4fe1da32943d3cc9badf7e40a2fbfe36bc9c
35840 <osku1993@luukku.com>
2020-10-28 10:55:43 +0200

    'jotain'

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const catRoute = require('./routes/catRoute');
+const app = express();
+
+const port = 3000;
+
+app.use('/cat', catRoute);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
